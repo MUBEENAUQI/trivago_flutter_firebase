c195f26c33ab8702aba05795147aed51492c2ecc
mubeenaTechknocks <67516058+mubeenaTechknocks@users.noreply.github.com>
2020-09-24 01:36:39 +0530

    trivago

---
+++ lib/authservice.dart
@@ -0,0 +1,37 @@
+import "package:firebase_auth/firebase_auth.dart";
+import 'package:trivago/models/user.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Usercustom _userfromfirebase(User user) {
+    return user != null ? Usercustom(uid: user.uid) : null;
+  }
+
+  Stream<Usercustom> get user {
+    return _auth
+        .authStateChanges()
+        //.map((User user) => _userfromfirebase(user));
+        .map(_userfromfirebase);
+  }
+
+  Future signInAnon() async {
+    try {
+      UserCredential result = await _auth.signInAnonymously();
+      User user = result.user;
+      return _userfromfirebase(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
